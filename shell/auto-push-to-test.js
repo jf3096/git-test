bffc3d14a57ae3657e096ce87a9b19210c41c012
艾伦 <529360897@qq.com>
2017-09-10 01:37:00 +0800

    first commit!

--- shell/auto-push-to-test.js
+++ shell/auto-push-to-test.js
@@ -1,8 +1,17 @@
 const git = require('simple-git');
 
 git()
+    .outputHandler(function (command, stdout, stderr) {
+        stdout.pipe(process.stdout);
+        stderr.pipe(process.stderr);
+    })
+    .pull(function (err, update) {
+        if (update && update.summary.changes) {
+            require('child_process').exec('npm restart');
+        }
+    })
     .add('./*')
     .commit("first commit!")
-    .push(['-u', 'origin', 'test'], function () {
+    .push(['--force', 'origin', 'test'], function () {
         // done.
     });
