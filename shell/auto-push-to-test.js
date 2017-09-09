212e63845c58d40cdcedccda07d953a7912ee156
艾伦 <529360897@qq.com>
2017-09-10 01:34:30 +0800

    first commit!

--- shell/auto-push-to-test.js
+++ shell/auto-push-to-test.js
@@ -3,6 +3,6 @@ const git = require('simple-git');
 git()
     .add('./*')
     .commit("first commit!")
-    .push(['-u', 'origin', 'master'], function () {
+    .push(['-u', 'origin', 'test'], function () {
         // done.
     });
