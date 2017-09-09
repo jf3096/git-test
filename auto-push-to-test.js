6efa86285ef0516fa57ea60f5bce80d66d1c88a9
艾伦 <529360897@qq.com>
2017-09-10 01:27:14 +0800

    auto commit

--- auto-push-to-test.js
+++ auto-push-to-test.js
@@ -3,6 +3,5 @@ const git = require('simple-git');
 git()
     .add('./*')
     .commit('auto commit')
-    .addRemote('origin', 'https://github.com/jf3096/git-test.git')
     .push(['-u', 'origin', 'test'], function () {
     });
